7eac3eff412268ceb350d98627be48618a695efa
dineshkush <kushwahd18@gmail.com>
2023-09-20 22:07:11 +0530

    new changes

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import Footer from './components/footer/Footer';
+import Header from './components/header/Header';
+
+function App() {
+  return (
+    <>
+    <Header />
+    <Footer />
+    </>
+  );
+}
+
+export default App;
