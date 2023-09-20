68303bee22cfc5dd3779d9585827edac2aa13234
dineshkush <kushwahd18@gmail.com>
2023-09-20 23:45:34 +0530

    creating header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import "./Header.css";
 
 function Header() {
   return (
-    <div>
-      
-    </div>
+    <header>
+        <div className="container">
+            <div className="row">
+                <div className="col-lg-2">
+                    <div className="logo">
+                        <h1>Web Digital <span>Work</span></h1>
+                    </div>
+                </div>
+                <div className="col-lg-8">
+
+                </div>
+                <div className="col-lg-2">
+
+                </div>
+            </div>
+        </div>
+    </header>
   )
 }
 
